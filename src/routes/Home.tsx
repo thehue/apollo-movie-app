66f887a505c21b1fabd5732be6ddae27b016ec6b
wonderotter <gouk32@naver.com>
2021-03-11 17:49:22 +0900

    Test useQuery

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,7 +1,43 @@
 import React from 'react';
+import { gql, useQuery } from '@apollo/client';
+
+interface Movie {
+  id: number;
+  medium_cover_image: string;
+}
+
+interface Movies {
+  movies: Movie[];
+}
+
+const GET_MOVIES = gql`
+  query {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
 
 function Home() {
-  return <div>Home</div>;
+  const { loading, data } = useQuery<Movies>(GET_MOVIES);
+
+  return (
+    <div>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <div>
+          {data &&
+            data.movies.map((movie) => (
+              <div key={movie.id}>
+                <img src={movie.medium_cover_image} alt="img" />
+              </div>
+            ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Home;
